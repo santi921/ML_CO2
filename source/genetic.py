6ad18d22f1f25e829cf1b8e3dbc997e233d3fdfd
Santiago Vargas <santi92@r398.ib.bridges2.psc.edu>
2021-12-08 20:58:04 -0500

    changed model to algo in parser

--- source/genetic.py
+++ source/genetic.py
@@ -317,17 +317,17 @@ class optimizer_genetic(object):
         print(pop_loss_temp)
 
         while len(parent_ind) <= int(successful_mol_count * ratio_children - 1):
-            boltz = True
+            log = False
             #print('draw')
-            draw = draw_from_pop_dist(pop_loss_temp, boltz = boltz)
-            #if (parent_ind.count(draw) == 0):
-            parent_ind.append(draw)
-            parent_gen_loss.append(pop_loss[draw])
-            #print('draw succ')
-            #if (log == False):
-            #    pop_loss_temp[draw] = 0.01
-            #else:
-            #    pop_loss_temp[draw] = 1.1
+            draw = draw_from_pop_dist(pop_loss_temp, log=log)
+            if (parent_ind.count(draw) == 0):
+                parent_ind.append(draw)
+                parent_gen_loss.append(pop_loss[draw])
+                #print('draw succ')
+                if (log == False):
+                    pop_loss_temp[draw] = 0.01
+                else:
+                    pop_loss_temp[draw] = 1.1
 
         if len(parent_ind) % 2 == 1: # chop off member from parent gen if odd numbered
             parent_ind = parent_ind[0:-1]
@@ -388,7 +388,7 @@ class optimizer_genetic(object):
 
             print("mutation...")
             for i in pop_new:
-                pop_mut_new.append(random_mutation(i, mut_chance=0.1))
+                pop_mut_new.append(random_mutation(i, mut_chance=0.2))
 
             self.population_sample = pop_mut_new
 
@@ -510,7 +510,7 @@ if __name__ == "__main__":
     print(":" * 50)
     print("Mean Loss array")
     print(":" * 50)
-    print(mean_loss)
+    #print(mean_loss)
     final_molecules = opt.population_sample
     final_molecules_selfies = [opt.vect_to_struct(i) for i in final_molecules]
     print("number of molecules at the end: " + str(len(final_molecules_selfies)))
