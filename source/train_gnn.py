8bcfd693490f5834e6d744fcb8504b8c411326c4
santi921 <santiagovargas921@gmail.com>
2022-02-07 12:11:19 -0500

    gnn archi

--- source/train_gnn.py
+++ source/train_gnn.py
@@ -23,8 +23,6 @@ if __name__ == "__main__":
           epochs = 50, callbacks = [es])
     model_loss = model.evaluate(loader_test.load(), steps=loader_test.steps_per_epoch)
     print("Done. Test loss: {}".format(model_loss))
-
-
     y_test = [loader_test.dataset[i]["y"] for i in range(len(loader_test))]
     y_test_pred = model.predict(loader_test.load(), verbose = 1, steps=loader_test.steps_per_epoch)
     print(r2_score(y_test_pred, y_test))
